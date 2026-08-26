90d8bb4ade7f1540b59ec50a754da990483233f8
CobyBoy <aboutacoby@gmail.com>
2026-08-25 22:51:54 -0300

    chore: add env config

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -2,10 +2,16 @@ import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
+import { environment } from './core/environments/environment.development';
+import { API_URL } from './core/config/api.token';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
-    provideRouter(routes)
-  ]
+    provideRouter(routes),
+    {
+      provide: API_URL,
+      useValue: environment.apiUrl,
+    },
+  ],
 };

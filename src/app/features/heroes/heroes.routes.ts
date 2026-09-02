6637cfc2f7d4b6695028f2bf64bbf38d7ddda6b9
CobyBoy <aboutacoby@gmail.com>
2026-09-02 13:44:05 -0300

    chore: add titles to routes

--- src/app/features/heroes/heroes.routes.ts
+++ src/app/features/heroes/heroes.routes.ts
@@ -3,14 +3,17 @@ import { Routes } from '@angular/router';
 export const HEROES_ROUTES: Routes = [
   {
     path: '',
+    title: 'Heroes | RIU',
     loadComponent: () => import('./pages/hero-list/hero-list').then((m) => m.HeroList),
   },
   {
     path: 'new',
+    title: 'Agregar héroe | RIU',
     loadComponent: () => import('./pages/hero-form/hero-form').then((m) => m.HeroForm),
   },
   {
     path: ':id/edit',
+    title: 'Editar héroe | RIU',
     loadComponent: () => import('./pages/hero-form/hero-form').then((m) => m.HeroForm),
   }
 ];
